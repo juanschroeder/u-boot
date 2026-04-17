f53238cb9c0bee7b589572a7c0261b12d394d1bc
Juan Schroeder <jcschroeder@gmail.com>
2026-04-17 17:46:37 +0200

    openhwgroup: cvwsoc: virt: adding virtual platform for QEMU/simulation.

---
+++ board/openhwgroup/cvwsoc_virt/cvwsoc_virt.c
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Copyright (C) 2026, Juan Schroeder <jcschroeder@gmail.com>
+ */
+
+#include <dm.h>
+#include <dm/ofnode.h>
+#include <env.h>
+#include <fdtdec.h>
+#include <image.h>
+#include <log.h>
+#include <spl.h>
+#include <init.h>
+#include <usb.h>
+#include <virtio_types.h>
+#include <virtio.h>
+#include <configs/openhwgroup_cvwsoc_virt.h>
+#include <../common/cvwsoc_common.h>
+
+int board_init(void) {
+
+    //vga_init(CVWSOC_VGA_ADDR, CVWSOC_FB_ADDR, CVWSOC_FB_WIDTH, CVWSOC_FB_HEIGHT, 
+    //    CVWSOC_VGA_CLK_DIV);
+    //vga_fb_init(CVWSOC_FB_ADDR, CVWSOC_FB_WIDTH, CVWSOC_FB_HEIGHT);
+
+    return 0;
+}
+
+
+int ft_board_setup(void *fdt, struct bd_info *bd) {
+    return 0;
+}
