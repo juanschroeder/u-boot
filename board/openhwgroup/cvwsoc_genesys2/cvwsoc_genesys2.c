22f50302ff4770ddfc3aef98296456676f1e7093
Juan Schroeder <jcschroeder@gmail.com>
2026-05-30 23:03:26 +0200

    temp commit.

--- board/openhwgroup/cvwsoc_genesys2/cvwsoc_genesys2.c
+++ board/openhwgroup/cvwsoc_genesys2/cvwsoc_genesys2.c
@@ -41,9 +41,9 @@ int board_init(void) {
             return 0;
 
         vga_init(CVWSOC_VGA_ADDR,
-                	fb_base,
-	                &vga_mode_320x240_in_640x480_100mhz);        
-        vga_fb_init(fb_base, ui_width, ui_height);
+                    fb_base,
+                    &vga_mode_320x240_in_640x480_100mhz);        
+        //vga_fb_init(fb_base, ui_width, ui_height);
 
     }
 
--- include/configs/openhwgroup_cvwsoc_nexysa7.h
+++ include/configs/openhwgroup_cvwsoc_nexysa7.h
@@ -6,11 +6,5 @@
 
 
 #define CVWSOC_VGA_ADDR             0x100B0000
-#define CVWSOC_VGA_CLK_DIV          0x4
-#define CVWSOC_FB_ADDR              0x87500000
-#define CVWSOC_FB_HEIGHT            240
-#define CVWSOC_FB_WIDTH             320
-#define CVWSOC_FB_SIZE			  (CVWSOC_FB_WIDTH * CVWSOC_FB_HEIGHT * 2)
-
 
 #endif
